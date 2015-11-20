f1797e0282316a6f765cbcdeaaa7b6ad304fe19c
Jesse Rogers <djrquazz@gmail.com>
2015-11-19 22:36:24 -0500

    Banks implementation

--- bank.cc
+++ bank.cc
@@ -1,9 +1,34 @@
 #include "bank.h"
 
-Bank::Bank( unsigned int numStudents ) : numStudents( numStudents ) {}
+Bank::Bank( unsigned int numStudents ) :
+  numStudents( numStudents ),
+  accounts( new Account[numStudents] )
+{
+  for ( unsigned int i = 0; i < numStudents; i++ ) {
+      accounts[i].amount = 0;
+  }
+}
+
+Bank::~Bank() {
+    delete [] accounts;
+}
 
 void Bank::deposit( unsigned int id, unsigned int amount ) {
+    Account &account = accounts[id];
+
+    account.amount += amount;
+
+    if ( ! account.bench.empty() && account.amount >= account.withdrawlIntent )
+        account.bench.signal();
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount ) {
+    Account &account = accounts[id];
+
+    if ( account.amount < amount ) {
+        account.withdrawlIntent = amount;
+        account.bench.wait();
+    }
+
+    account.amount -= amount;
 }
--- bank.h
+++ bank.h
@@ -3,8 +3,14 @@
 
 _Monitor Bank {
     const unsigned int numStudents;
+    struct Account {
+        unsigned int amount;
+        unsigned int withdrawlIntent;
+        uCondition bench;
+    } *accounts;
   public:
     Bank( unsigned int numStudents );
+    ~Bank();
     void deposit( unsigned int id, unsigned int amount );
     void withdraw( unsigned int id, unsigned int amount );
 };
